8b977eed742809d043cbeb3a1a15ed4a647b3e1b
Snepx <153798266+Snepx@users.noreply.github.com>
2025-02-02 08:32:37 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+// Javascript para adicionar interatividade (exemplo)
+const acaiSection = document.getElementById('acai');
+
+acaiSection.addEventListener('click', () => {
+  alert('Você clicou na seção sobre o açaí!');
+});
